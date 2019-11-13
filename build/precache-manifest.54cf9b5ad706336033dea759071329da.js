9a6f4d3178d2228fbd5a91b40ec940f51858e438
JFaarinen <juho.faarinen@gmail.com>
2019-11-13 14:58:49 +0200

    Add files via upload

---
+++ build/precache-manifest.54cf9b5ad706336033dea759071329da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15156beffc16c8a30f66c1f7abf7a702",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4303acc300b26362fc7e",
+    "url": "/static/css/main.d7c47903.chunk.css"
+  },
+  {
+    "revision": "9cf798c52e45e8e8042e",
+    "url": "/static/js/2.d3c593ce.chunk.js"
+  },
+  {
+    "revision": "4303acc300b26362fc7e",
+    "url": "/static/js/main.6f777ff4.chunk.js"
+  },
+  {
+    "revision": "bd15de2c81a3438b90f1",
+    "url": "/static/js/runtime~main.11bda596.js"
+  }
+]);
